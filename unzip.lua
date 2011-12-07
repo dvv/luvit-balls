d45dd269f49ca7a2f26ca4762298751f394c99ce
Vladimir Dronnikov <dronnikov@gmail.com>
2011-12-07 13:02:36 +0000

    extraction

--- unzip.lua
+++ unzip.lua
@@ -6,8 +6,16 @@ end
 local sub, byte, char
 do
   local _table_0 = require('string')
-  sub, byte, char = _table_0.sub, _table_0.byte, _table_0.char
+  sub = _table_0.sub
+  byte = _table_0.byte
+  char = _table_0.char
 end
+local insert
+do
+  local _table_0 = require('table')
+  insert = _table_0.insert
+end
+local Zlib = require('./zlib')
 local MAGIC_NUMBER = 0x04034b50
 local BigEndianBinaryStream
 BigEndianBinaryStream = (function()
@@ -62,65 +70,100 @@ BigEndianBinaryStream = (function()
   _base_0.__class = _class_0
   return _class_0
 end)()
-local read
-read = function(text)
-  local buffer = BigEndianBinaryStream(text)
-  if buffer:get_int() ~= MAGIC_NUMBER then
-    error('File is not a Zip file')
-  end
-  buffer:rewind()
-  local index = 0
-  local entries = { }
-  while buffer:get_int() == MAGIC_NUMBER do
-    local entry = { }
-    index = index + 1
-    entry.index = index
-    local version = buffer:get_short()
-    local flags = buffer:get_short()
-    if band(flags, 0x01) == 0x01 then
-      error('File contains encrypted entry. Not supported')
-    end
-    if band(flags, 0x0800) == 0x0800 then
-      error('File is using UTF8. Not supported')
-    end
-    if band(flags, 0x0008) == 0x0008 then
-      error('File is using bit 3 trailing data descriptor. Not supported')
-    end
-    entry.comp_method = buffer:get_short()
-    entry.mtime = buffer:get_int()
-    entry.crc = buffer:get_int()
-    entry.comp_size = buffer:get_int()
-    entry.size = buffer:get_int()
-    if entry.comp_size == 0xFFFFFFFF or entry.size == 0xFFFFFFFF then
-      error('File is using Zip64 (4gb+ file size). Not supported')
-    end
-    local name_len = buffer:get_short()
-    local extra_len = buffer:get_short()
-    entry.name = buffer:get_string(name_len)
-    entry.extra = buffer:get_string(extra_len)
-    entry.data = buffer:get_string(entry.comp_size)
-    if type(entry.data) == 'string' then
-      entries[entry.name] = entry
+local Zip_prototype = {
+  get = function(self, name)
+    local entry = self[name]
+    if entry.comp_method > 0 then
+      local ok, text = pcall(Zlib.inflate(-15), entry.data, 'finish')
+      if ok then
+        return text
+      else
+        return ''
+      end
     else
+      return entry.data
+    end
+  end,
+  read = function(self, text)
+    local buffer = BigEndianBinaryStream(text)
+    if buffer:get_int() ~= MAGIC_NUMBER then
+      error('File is not a Zip file')
+    end
+    buffer:rewind()
+    local index = 0
+    self.entries = { }
+    while buffer:get_int() == MAGIC_NUMBER do
+      local entry = { }
+      index = index + 1
+      entry.index = index
+      local version = buffer:get_short()
+      local flags = buffer:get_short()
+      if band(flags, 0x01) == 0x01 then
+        error('File contains encrypted entry. Not supported')
+      end
+      if band(flags, 0x0800) == 0x0800 then
+        error('File is using UTF8. Not supported')
+      end
+      if band(flags, 0x0008) == 0x0008 then
+        error('File is using bit 3 trailing data descriptor. Not supported')
+      end
+      entry.comp_method = buffer:get_short()
+      entry.mtime = buffer:get_int()
+      entry.crc = buffer:get_int()
+      entry.comp_size = buffer:get_int()
+      entry.size = buffer:get_int()
+      if entry.comp_size == 0xFFFFFFFF or entry.size == 0xFFFFFFFF then
+        error('File is using Zip64 (4gb+ file size). Not supported')
+      end
+      local name_len = buffer:get_short()
+      local extra_len = buffer:get_short()
+      entry.name = buffer:get_string(name_len)
+      entry.extra = buffer:get_string(extra_len)
+      entry.data = buffer:get_string(entry.comp_size)
+      if type(entry.data) == 'string' then
+        self.entries[entry.name] = entry
+        insert(self.entries, entry)
+      else
+        break
+      end
+    end
+    return self.entries
+  end
+}
+local Zip = {
+  new = function()
+    local self = { }
+    return setmetatable(self, {
+      __index = Zip_prototype
+    })
+  end
+}
+local Path = require('path')
+local Fs = require('./util')
+local zip = Zip.new()
+zip = zip.read(Fs.read_file_sync('test.zip', 'utf8'))
+local Fiber = require('fiber')
+Fiber.new(function(resume, wait)
+  local err = nil
+  local i = nil
+  local k = nil
+  local v = nil
+  for i, v in ipairs(zip) do
+    k = v.name
+    Fs.mkdir_p(Path.dirname(k), '0755', resume)
+    err = wait()
+    if err then
+      p(k, err)
       break
     end
+    if v.size > 0 and v.comp_size > 0 then
+      local text = zip:get(k)
+      Fs.write_file(k, text, resume)
+      err = wait()
+      if err then
+        p(k, err)
+        break
+      end
+    end
   end
-  return entries
-end
-local file = require('fs').read_file_sync('test.zip', 'utf8')
-local zip = read(file)
-local ffi = require('ffi')
-ffi.cdef([[unsigned long compressBound(unsigned long sourceLen);
-int compress2(uint8_t *dest, unsigned long *destLen,
-      const uint8_t *source, unsigned long sourceLen, int level);
-int uncompress(uint8_t *dest, unsigned long *destLen,
-       const uint8_t *source, unsigned long sourceLen);
-]])
-local zlib = ffi.load(ffi.os == 'Windows' and "zlib1" or 'z')
-local entry = zip['sockjs-sockjs-client-20c0df3/Makefile']
-local data = entry.data
-entry.data = nil
-print(data)
-local Zlib = require('./zlib')
-local s = Zlib.inflate(-15)(data, 'finish')
-p(s)
+end)
--- unzip.moon
+++ unzip.moon
@@ -1,5 +1,8 @@
 import band from require 'bit'
 import sub, byte, char from require 'string'
+import insert from require 'table'
+
+Zlib = require './zlib'
 
 MAGIC_NUMBER = 0x04034b50
 
@@ -36,78 +39,116 @@ class BigEndianBinaryStream
     @index = @index + len
     res
 
-read = (text) ->
-  buffer = BigEndianBinaryStream text
+Zip_prototype =
 
-  error 'File is not a Zip file' if buffer\get_int() != MAGIC_NUMBER
-  buffer\rewind()
-
-  index = 0
-  entries = {}
-
-  while buffer\get_int() == MAGIC_NUMBER
-    --error 'Bad signature'
-
-    entry = {}
-
-    index = index + 1
-    entry.index = index
-    version = buffer\get_short()
-    flags = buffer\get_short()
-
-    if band(flags, 0x01) == 0x01
-      error 'File contains encrypted entry. Not supported'
-
-    if band(flags, 0x0800) == 0x0800
-      error 'File is using UTF8. Not supported'
-
-    if band(flags, 0x0008) == 0x0008
-      error 'File is using bit 3 trailing data descriptor. Not supported'
-
-    entry.comp_method = buffer\get_short()
-    entry.mtime = buffer\get_int()
-    entry.crc = buffer\get_int()
-    entry.comp_size = buffer\get_int()
-    entry.size = buffer\get_int()
-
-    if entry.comp_size == 0xFFFFFFFF or entry.size == 0xFFFFFFFF
-      error 'File is using Zip64 (4gb+ file size). Not supported'
-
-    name_len = buffer\get_short()
-    extra_len = buffer\get_short()
-
-    entry.name = buffer\get_string name_len
-    entry.extra = buffer\get_string extra_len
-
-    entry.data = buffer\get_string entry.comp_size
-    --assert(#entry.data == entry.comp_size)
-    --entry.data = ''
-    --entry.ptr = buffer\index
-    --entry.data = sub buffer.buffer, buffer.index - 32, buffer.index + entry.comp_size + 32 - 1
-
-    if type(entry.data) == 'string'
-      entries[entry.name] = entry
+  get: (name) =>
+    entry = self[name]
+    if entry.comp_method > 0
+      ok, text = pcall Zlib.inflate(-15), entry.data, 'finish'
+      if ok
+        text
+      else
+        ''
     else
-      break
+      entry.data
 
-  entries
 
--- export
---return Unzip
+  read: (text) =>
+    buffer = BigEndianBinaryStream text
+
+    error 'File is not a Zip file' if buffer\get_int() != MAGIC_NUMBER
+    buffer\rewind()
+
+    index = 0
+    self.entries = {}
+
+    while buffer\get_int() == MAGIC_NUMBER
+      --error 'Bad signature'
+
+      entry = {}
+
+      index = index + 1
+      entry.index = index
+      version = buffer\get_short()
+      flags = buffer\get_short()
+
+      if band(flags, 0x01) == 0x01
+        error 'File contains encrypted entry. Not supported'
+
+      if band(flags, 0x0800) == 0x0800
+        error 'File is using UTF8. Not supported'
+
+      if band(flags, 0x0008) == 0x0008
+        error 'File is using bit 3 trailing data descriptor. Not supported'
+
+      entry.comp_method = buffer\get_short()
+      entry.mtime = buffer\get_int()
+      entry.crc = buffer\get_int()
+      entry.comp_size = buffer\get_int()
+      entry.size = buffer\get_int()
+
+      if entry.comp_size == 0xFFFFFFFF or entry.size == 0xFFFFFFFF
+        error 'File is using Zip64 (4gb+ file size). Not supported'
+
+      name_len = buffer\get_short()
+      extra_len = buffer\get_short()
+
+      entry.name = buffer\get_string name_len
+      entry.extra = buffer\get_string extra_len
+
+      entry.data = buffer\get_string entry.comp_size
+      --assert(#entry.data == entry.comp_size)
+      --entry.data = ''
+      --entry.ptr = buffer\index
+      --entry.data = sub buffer.buffer, buffer.index - 32, buffer.index + entry.comp_size + 32 - 1
+
+      if type(entry.data) == 'string'
+        -- index by name
+        self.entries[entry.name] = entry
+        -- index by position
+        insert self.entries, entry
+      else
+        break
+
+    self.entries
+
+Zip =
+
+  new: () ->
+    self = {}
+    setmetatable self, __index: Zip_prototype
 
 -----
-file = require('fs').read_file_sync('test.zip', 'utf8')
-zip = read(file)
---p(zip)
---for k, v in ipairs zip
---  p(k)
+Path = require 'path'
+Fs = require('./util')
 
-entry = zip['sockjs-sockjs-client-20c0df3/Makefile']
---entry = zip['sockjs-sockjs-client-20c0df3/.gitignore']
-data = entry.data
-entry.data = nil
---p entry
-print data
-Zlib = require './zlib'
-s = Zlib.inflate(-15) data, 'finish'
-p(s)
+-- TODO: make streaming
+
+zip = Zip.new()
+zip = zip.read(Fs.read_file_sync('test.zip', 'utf8'))
+--p(zip)
+
+Fiber = require 'fiber'
+Fiber.new (resume, wait) ->
+  err = nil
+  i = nil
+  k = nil
+  v = nil
+  --for k, v in ipairs zip
+  for i, v in ipairs zip
+    k = v.name
+    Fs.mkdir_p Path.dirname(k), '0755', resume
+    err = wait()
+    if err
+      p k, err
+      break
+    if v.size > 0 and v.comp_size > 0
+      text = zip\get k
+      Fs.write_file k, text, resume
+      err = wait()
+      if err
+        p k, err
+        break
+
+--p zip.get 'sockjs-sockjs-client-20c0df3/.gitignore'
+--p zip.get 'sockjs-sockjs-client-20c0df3/version'

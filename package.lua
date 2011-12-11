64e787b8751524d7b061e123fd65be9501599c3e
Vladimir Dronnikov <dronnikov@gmail.com>
2011-12-11 22:23:21 +0400

    added manifest and readme

---
+++ package.lua
@@ -0,0 +1,9 @@
+return {
+  name = "balls",
+  version = "0.0.1",
+  description = "Utilities to deal with zipballs and tarballs",
+  author = "Vladimir Dronnikov <dronnikov@gmail.com>",
+  dependencies = {
+    ["meta-fs"] = "https://github.com/luvit/meta-fs/zipball/master",
+  },
+}
